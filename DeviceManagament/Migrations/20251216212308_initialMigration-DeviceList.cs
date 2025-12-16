3eaf799c8bd84dfc1059ffbe569f59a9212336e4
hmp012 <hugo.penarrubia@lego.com>
2025-12-16 22:46:33 +0100

    Create db (#8)
    * created compose and dockerfile
    
    * fixed compose, added first migration
    Database working but empty

---
+++ DeviceManagament/Migrations/20251216212308_initialMigration-DeviceList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeviceManagament.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialMigrationDeviceList : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "devices");
+
+            migrationBuilder.CreateTable(
+                name: "Devices",
+                schema: "devices",
+                columns: table => new
+                {
+                    SerialNumber = table.Column<Guid>(type: "uuid", nullable: false),
+                    ModelId = table.Column<string>(type: "text", nullable: false),
+                    ModelName = table.Column<string>(type: "text", nullable: false),
+                    Manufacturer = table.Column<string>(type: "text", nullable: false),
+                    PrimaryUser = table.Column<string>(type: "text", nullable: false),
+                    OperatingSystem = table.Column<string>(type: "text", nullable: false),
+                    DeviceType = table.Column<int>(type: "integer", nullable: false),
+                    DeviceStatus = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Devices", x => x.SerialNumber);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_SerialNumber",
+                schema: "devices",
+                table: "Devices",
+                column: "SerialNumber",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Devices",
+                schema: "devices");
+        }
+    }
+}
