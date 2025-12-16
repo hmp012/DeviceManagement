343cf87823545c7990d9cd276fa33f3be9a180b2
hmp012 <hugo.penarrubia@lego.com>
2025-12-16 23:09:23 +0100

    implemented device repository (#9)

---
+++ DeviceManagament/Repositories/DeviceRepository.cs
@@ -0,0 +1,35 @@
+using DeviceManagament.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace DeviceManagament.Repositories;
+
+public interface IDeviceRepository
+{
+    public Task<Device> AddDevice(Device device);
+    public Task<Device?> GetDevice(Device device);
+    public Task<Device> UpdateDevice(Device device);
+}
+
+public class DeviceRepository(DeviceManagament.Database.DeviceManagerDbContext dbContext) : IDeviceRepository
+{
+    public async Task<Device> AddDevice(Device device)
+    {
+        EntityEntry<Device> addedEntity = await dbContext.Devices.AddAsync(device);
+        await dbContext.SaveChangesAsync();
+        return addedEntity.Entity;
+    }
+
+    public async Task<Device?> GetDevice(Device device)
+    {
+        return await dbContext.Devices
+            .FirstOrDefaultAsync(d => d.SerialNumber == device.SerialNumber);
+    }
+
+    public async Task<Device> UpdateDevice(Device device)
+    {
+        EntityEntry<Device> updatedEntity = dbContext.Devices.Update(device);
+        await dbContext.SaveChangesAsync();
+        return updatedEntity.Entity;
+    }
+}
